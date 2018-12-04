70c558087ca66f5a29ed9ad99ea8b69fab2bd72f
JeroenFaasse <99045518@mydavinci.nl>
2018-12-04 14:49:42 +0100

    .

--- adventure.js
+++ adventure.js
@@ -5,7 +5,8 @@ var btn1 = document.getElementById('button1');
 var btn2 = document.getElementById('button2');
 var btn3 = document.getElementById('button3');
 var inventory = document.getElementById('inventoryItem');
-
+var clickAudio = new Audio('snd/click.mp3');
+var audio = new Audio('snd/soundtrack.mp3');
 
 title.innerHTML = "VIP Extraction";
 description.innerHTML = "Your mission is to extract a VIP out of the Vietnamese jungle. You must succeed without being spotted.";
@@ -15,6 +16,9 @@ btn2.style.display = "none";
 btn3.style.display = "none";
 
 btn1.onclick = function(){
+	audio.play();
+	audio.loop = true;
+	clickAudio.play();
 	btn1.innerHTML = "Walk left";
 	btn2.style.display = "inline";
 	btn3.style.display = "inline";
@@ -24,13 +28,22 @@ btn1.onclick = function(){
 	description.innerHTML = "Try to get out of here. The VIP has three possible locations. Infiltrate all 3 objectives and extract the VIP when found. Shoot only if necessary. You are armed with a pistol and 2 magazine's.";
 	inventory.style.display = "inline";
 	inventory.src = "img/16.png";
-	document.getElementById('game-container').style.backgroundImage = "url('img/start.jpg')";
+	bg.style.backgroundImage = "url('img/start.jpg')";
 	btn1.onclick = function(){
 		walkLeft();
 	}
 }
 
 function walkLeft(){
+	btn1.innerHTML = "Walk left";
+	btn2.innerHTML = "Walk forward";
+	btn3.innerHTML = "Walk right";
+	title.innerHTML = "Next up is objective ALPHA.";
+	description.innerHTML = "Watch out for Tango's. You do not want them to see you.";
+	bg.style.backgroundImage = "url('img/left.jpg')";
+}
+
+function walkForward(){
 	btn1.innerHTML = "Walk left";
 	btn2.innerHTML = "Walk forward";
 	btn3.innerHTML = "Walk right";
